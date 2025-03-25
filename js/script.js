ed4eee36f2015d06a686e9bfa74e75f6924cc8ab
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-03-24 22:22:32 -0300

    adicionando switch
    adicionando darkmode e lightmode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,70 @@
 var btnSobre = document.querySelector(".sobre")
 
+<<<<<<< Updated upstream
 btnSobre.addEventListener
+=======
+const sobre = document.querySelector(".sobreDiv");
+const missao = document.querySelector(".missaoDiv");
+const visao = document.querySelector(".visaoDiv");
+const valores = document.querySelector(".valoresDiv");
+
+
+btnSobre.addEventListener("click", ()=> {
+    sobre.style.display = "flex";
+    missao.style.display = "none";
+    visao.style.display = "none";
+    valores.style.display = "none";
+})
+
+btnMissao.addEventListener("click", ()=> {
+    sobre.style.display = "none";
+    missao.style.display = "flex";
+    visao.style.display = "none";
+    valores.style.display = "none";
+})
+
+btnVisao.addEventListener("click", ()=> {
+    sobre.style.display = "none";
+    missao.style.display = "none";
+    visao.style.display = "flex";
+    valores.style.display = "none";
+})
+
+btnValores.addEventListener("click", ()=> {
+    sobre.style.display = "none";
+    missao.style.display = "none";
+    visao.style.display = "none";
+    valores.style.display = "flex";
+})
+
+
+const darkBtn = document.querySelector(".darkBtn")
+const lightBtn = document.querySelector(".lightBtn")
+const body = document.body
+
+darkBtn.addEventListener("click", ()=> {
+    body.classList.remove("lightMode");
+    body.classList.add("darkMode");
+
+})
+
+
+lightBtn.addEventListener("click", ()=> {
+    body.classList.remove("darkMode");
+    body.classList.add("lightMode");
+
+})
+
+
+
+// window.addEventListener("scroll", function () {
+//     var navbar = document.getElementById("navbar");
+    
+//     if (window.scrollY > 100) { // Altere '100' para o valor desejado
+//       navbar.classList.add("scrolled");
+//     } else {
+//       navbar.classList.remove("scrolled");
+//     }
+//   });
+  
+>>>>>>> Stashed changes
