75c5a950d4968339ad14f8ff400813317287d697
MATHEUS BEZERRA DE SOUZA <matheus.bsouza22@senacsp.edu.br>
2025-03-26 21:54:05 -0300

    final

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-
 const btnSobre = document.querySelector(".sobre")
 const btnMissao = document.querySelector(".missao")
 const btnVisao = document.querySelector(".visao")
 const btnValores = document.querySelector(".valores")
 
-
 const sobre = document.querySelector(".sobreDiv");
 const missao = document.querySelector(".missaoDiv");
 const visao = document.querySelector(".visaoDiv");
 const valores = document.querySelector(".valoresDiv");
-
 
 
 btnSobre.addEventListener("click", ()=> {
@@ -59,3 +56,25 @@ lightBtn.addEventListener("click", ()=> {
 })
 
 
+
+window.addEventListener("scroll", function () {
+    var navbar = document.querySelector("nav");
+    
+    if (window.scrollY > 100) { // Altere '200' para o valor desejado
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+  
+  document.querySelector('.nav-link').addEventListener('click', function(e) {
+    e.preventDefault(); // Impede o comportamento padrão do link
+  
+    const target = document.querySelector(this.getAttribute('href'));
+    
+    // Faz a rolagem com o deslocamento de 100px
+    window.scrollTo({
+      top: target.offsetTop + 0, // Desce 100px a mais
+      behavior: 'smooth' // Rolagem suave
+    });
+  });
